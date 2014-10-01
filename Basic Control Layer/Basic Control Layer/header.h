209465011a8a69710bf82010f579ee9dc8bd1dd9
Floris497 <Floris@noxbox.nl>
2014-10-01 16:50:55 +0200

    added xCode project

---
+++ Basic Control Layer/Basic Control Layer/header.h
@@ -0,0 +1,13 @@
+//
+//  header.h
+//  Basic Control Layer
+//
+//  Created by Floris Fredrikze on 01/10/14.
+//  Copyright (c) 2014 MiniMind. All rights reserved.
+//
+
+#ifndef Basic_Control_Layer_header_h
+#define Basic_Control_Layer_header_h
+
+
+#endif
---
+++ Basic Control Layer/Basic Control Layer/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Basic Control Layer
+//
+//  Created by Floris Fredrikze on 01/10/14.
+//  Copyright (c) 2014 MiniMind. All rights reserved.
+//
+
+#include <stdio.h>
+#include "header.h"
+
+int main(int argc, const char * argv[]) {
+
+    
+    return 0;
+}
