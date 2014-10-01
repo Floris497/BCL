9b3222782b1a2bcd90c2344b94f76690f2bec0f8
Floris497 <Floris@noxbox.nl>
2014-10-01 20:25:15 +0200

    Added part of I2C manager

--- Basic Control Layer/Basic Control Layer/header.h
+++ Basic Control Layer/Basic Control Layer/header.h
@@ -10,14 +10,22 @@
 #define Basic_Control_Layer_header_h
 
 
-typedef struct I2CInterface {
-    uint8_t  deviceID;
-} I2CInterface;
+typedef struct I2CDevice {
+    uint8_t  I2C_ID;
+} I2CDevice;
 
+typedef struct I2CData {
+    I2CDevice device;
+    uint8_t command[10];
+    uint8_t commandLength;
+} I2CData;
 
-// script layer
+enum {
+    I2C_SEND_MODE,
+    I2C_RECIEVE_MODE
+};
 
-// driving
+// ---- script layer ---- //
 int moveForwardWithSpeed(uint8_t speed); //1 to 100
 int moveBackwardWithSpeed(uint8_t speed); //1 to 100
 int moveLeftWithSpeedAndIntensity(uint8_t speed,uint8_t intensity); // both 1 to 100
@@ -25,17 +33,22 @@ int moveRightWithSpeedAndIntensity(uint8_t speed,uint8_t intensity); // both 1 t
 int turnRight();
 int turnLeft();
 
-// access layer
+// ---- access layer ---- //
 void moveWithSpeed(int leftSpeed,int rightSpeed);
 
 
-// module layer
-void makeCommandFromSpeed(int leftSpeed,int rightSpeed,int *command);
-void sendI2CCommandTo(I2CInterface device,int *command,int length);
+
+// ---- module layer ---- //
+void makeCommandFromSpeed(int leftSpeed,int rightSpeed,I2CData data);
+void sendI2CCommand(I2CData data);
+void getI2CData(I2CData data);
 
 
-// support layer
+// ---- support layer ---- //
 
+
+// ---- I2C Manager ---- //
+void I2CTask(I2CData data,int I2C_MODE);
 
 
 #endif
