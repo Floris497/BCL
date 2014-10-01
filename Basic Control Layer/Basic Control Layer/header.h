7a56783961c7a6865dff21403ce5a01ca3efe040
Floris497 <Floris@noxbox.nl>
2014-10-01 17:56:22 +0200

    Added basic driving functions

--- Basic Control Layer/Basic Control Layer/header.h
+++ Basic Control Layer/Basic Control Layer/header.h
@@ -10,4 +10,32 @@
 #define Basic_Control_Layer_header_h
 
 
+typedef struct I2CInterface {
+    uint8_t  deviceID;
+} I2CInterface;
+
+
+// script layer
+
+// driving
+int moveForwardWithSpeed(uint8_t speed); //1 to 100
+int moveBackwardWithSpeed(uint8_t speed); //1 to 100
+int moveLeftWithSpeedAndIntensity(uint8_t speed,uint8_t intensity); // both 1 to 100
+int moveRightWithSpeedAndIntensity(uint8_t speed,uint8_t intensity); // both 1 to 100
+int turnRight();
+int turnLeft();
+
+// access layer
+void moveWithSpeed(int leftSpeed,int rightSpeed);
+
+
+// module layer
+void makeCommandFromSpeed(int leftSpeed,int rightSpeed,int *command);
+void sendI2CCommandTo(I2CInterface device,int *command,int length);
+
+
+// support layer
+
+
+
 #endif
